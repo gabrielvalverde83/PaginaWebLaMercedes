7620b65695bff3abef196e9b41c44c50a4b57084
Gabriel <gabrielvalverde83@hotmail.com>
2024-03-31 22:30:32 -0300

    subiendo archivos

---
+++ js/my-scripts.js
@@ -0,0 +1,48 @@
+'use stric'
+
+
+$(document).ready(function(){
+
+	////////////////////////////////////////////////////////
+	//abrir y cerrar menu en sitio responsivo ///////////
+	////////////////////////////////////////////////////////
+
+	$('.navbar-toggle').click(function(){
+		$('.header-menu').removeClass('menu-ubicacion');
+		$('.header-menu').addClass('menuResponsivo-ubicacion');
+	});
+
+	$('.menu-listado li').click(function(){
+		$('.header-menu').removeClass('menuResponsivo-ubicacion');
+		$('.header-menu').addClass('menu-ubicacion');
+	});
+	
+	$('.menu-cerrar h3').click(function(){
+		$('.header-menu').removeClass('menuResponsivo-ubicacion');
+		$('.header-menu').addClass('menu-ubicacion');
+	});
+
+
+	////////////////////////////////////////////////////////	
+	//boton para subir al inicio ///////////////////
+	////////////////////////////////////////////////////////
+
+ 	var $boton_top	= $('.to-top');
+ 	$boton_top.hide();
+
+ 	$(window).scroll(function(){
+ 		if ( $(this).scrollTop() > 300 ) {
+ 			$boton_top.fadeIn();
+ 		} else {
+ 			$boton_top.fadeOut();
+ 		}
+ 	});
+
+ 	$boton_top.click(function(event){
+ 		event.preventDefault();
+ 		$('html, body').animate({
+ 			scrollTop:0},1200)
+ 		return false;
+ 	});
+
+});
